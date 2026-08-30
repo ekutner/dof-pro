7796d312ea9df8558fee9005920f6c3cb1a078c4
Eran Kutner <5628151+ekutner@users.noreply.github.com>
2026-08-30 15:21:17 +0300

    Update version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.kutner.dofpro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     /**
